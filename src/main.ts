9b5b02f43269131e7f58eddc5311d9e3f844f686
Eugene Kalashnikov <ekalashnikov59@gmail.com>
2024-06-25 19:44:00 +0200

    9000

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT);
+  await app.listen(9000);
 }
 bootstrap();
